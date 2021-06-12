0ff07dc5d4cd2fcf0f49a18b7b8ff5f12a45f864
Guillermo <Sanguillermo98@gmail.com>
2021-06-11 17:46:25 -0700

    Updated action

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # DAILY_DIGEST=$(curl -s https://memo-stock-bot.herokuapp.com/daily | jq -r '.[] | "\(.name) \(.results) \n" ' | sed 's/"//g')
 # curl -s -X POST "https://discord.com/api/webhooks/852683075544809512/tl6icg-eP7u-gGIRRkuZGgzp94JpxHIhiHOfI4k2UgkgeB8bGSK8gxJQjZIDjbLh3nEe" -d "content=$DAILY_DIGEST"
-curl -s localhost:5000/daily | jq -r '.[] | "\(.name) \(.results) \n" ' | sed 's/"//g'
+curl -s localhost:5000/daily | jq -r '.[] | "\(.name) \n • \(.results) \n" ' | sed 's/"//g'
