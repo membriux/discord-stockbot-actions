01413b66985c888b41389b31c399b70de55b1cdd
Guillermo <Sanguillermo98@gmail.com>
2021-06-11 17:30:40 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-curl -s https://memo-stock-bot.herokuapp.com/daily | jq '.[] | "\(.name) \(.results)" '
+# curl -s https://memo-stock-bot.herokuapp.com/daily | jq '.[] | "\(.name) \(.results)" '
+curl -s localhost:5000/daily | jq -r '.[] | "\(.name) \(.results)" ' | sed 's/"//g'
